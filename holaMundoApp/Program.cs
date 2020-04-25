533ad007dcf906932d9b0eaa7b12ff6e82a5ac4f
juampavilla <juampavilla@gmail.com>
2020-04-25 17:04:06 -0300

    initial commit - clase 1

---
+++ holaMundoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace holaMundoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+
+            Console.WriteLine("mostrar el número:" + a);
+        }
+    }
+}
