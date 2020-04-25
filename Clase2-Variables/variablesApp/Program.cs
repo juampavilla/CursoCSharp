a56b53e05d8fea66f88e87b3086cf570fe346bdc
juampavilla <juampavilla@gmail.com>
2020-04-25 18:17:47 -0300

    Clase 2 variables ejercicio resuelto

---
+++ Clase2-Variables/variablesApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace variablesApp
+{
+    class Program
+    {
+        /** Mostremos por pantalla el mensaje: "¿Cuánto es 25 x 75 ?" **/
+        // Guardemos en una variable el valor 25
+        // Guardemos en otra variable el valor 75
+        // Realicemos una operación de multiplicación entre esas variables
+        // Y mostremos por pantalla: 
+                //"El resultado es:" + el valor de una variable con el resultado.
+        
+        // Aclaración: la operación de multiplicación en C# se define utilizando el *
+        static void Main(string[] args)
+        {
+            //inicialización de variables
+            int primerTermino = 25;
+            int segundoTermino = 75;
+
+            //multiplicación de las variables
+            int resultadoMult = primerTermino * segundoTermino;
+            
+            //salida en pantalla del resultado:
+            Console.WriteLine("El resultado es:" + resultadoMult);
+        }
+    }
+}
